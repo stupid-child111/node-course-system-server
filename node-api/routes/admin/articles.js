065da56e818ea7826bb063dbf575725a41fa3e73
stupid-child111 <3468619937@qq.com>
2025-10-12 16:15:27 +0800

    feat:实现文章详情查询接口

--- node-api/routes/admin/articles.js
+++ node-api/routes/admin/articles.js
@@ -30,4 +30,34 @@ router.get("/", async function (req, res) {
   }
 });
 
+/**
+ * 查询文章详情
+ * GET /admin/articles/:id
+ */
+router.get("/:id", async function (req, res) {
+  try {
+    // 获取文章 ID
+    const { id } = req.params;
+    const article = await Article.findByPk(id);
+    if (article) {
+      res.json({
+        status: true,
+        message: "查询文章详情成功",
+        data: article,
+      });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "文章未找到。",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "查询文章详情失败。",
+      errors: [error.message],
+    });
+  }
+});
+
 module.exports = router;
