a04614f96a240fe80432a3d09df25659c2dd8bbd
stupid-child111 <3468619937@qq.com>
2025-10-13 22:28:46 +0800

    fix:修复课程路由的删除部分代码

--- node-api/app.js
+++ node-api/app.js
@@ -11,6 +11,7 @@ const adminSettingsRouter = require("./routes/admin/settings")
 const adminUsersRouter = require("./routes/admin/users")
 const adminCoursesRouter = require('./routes/admin/courses');
 const adminChaptersRouter = require('./routes/admin/chapters');
+const adminChartsRouter = require('./routes/admin/charts');
 
 
 const app = express();
@@ -29,5 +30,6 @@ app.use('/admin/settings',adminSettingsRouter)
 app.use('/admin/users',adminUsersRouter)
 app.use('/admin/courses', adminCoursesRouter);
 app.use('/admin/chapters', adminChaptersRouter);
+app.use('/admin/charts', adminChartsRouter);
 
 module.exports = app;
