34490cf0e944eb29ca332687804980250ac1b214
stupid-child111 <3468619937@qq.com>
2025-10-13 22:39:27 +0800

    refactor:封装成功和失败的响应

---
+++ node-api/utils/responses.js
@@ -0,0 +1,65 @@
+/**
+ * 请求成功
+ * @param res
+ * @param message
+ * @param data
+ * @param code
+ */
+function success(res, message, data = {}, code = 200) {
+  res.status(code).json({
+    status: true,
+    message,
+    data,
+  });
+}
+
+/**
+ * 请求失败
+ * @param res
+ * @param error
+ */
+function failure(res, error) {
+  if (error.name === "SequelizeValidationError") {
+    const errors = error.errors.map((e) => e.message);
+    return res.status(400).json({
+      status: false,
+      message: "请求参数错误",
+      errors,
+    });
+  }
+
+  if (error.name === "BadRequestError") {
+    return res.status(400).json({
+      status: false,
+      message: "请求参数错误",
+      errors: [error.message],
+    });
+  }
+
+  if (error.name === "UnauthorizedError") {
+    return res.status(401).json({
+      status: false,
+      message: "认证失败",
+      errors: [error.message],
+    });
+  }
+
+  if (error.name === "NotFoundError") {
+    return res.status(404).json({
+      status: false,
+      message: "资源不存在",
+      errors: [error.message],
+    });
+  }
+
+  res.status(500).json({
+    status: false,
+    message: "服务器错误",
+    errors: [error.message],
+  });
+}
+
+module.exports = {
+  success,
+  failure,
+};
