80ac50e03cbbebce7452bb59f10f61b823d7538d
stupid-child111 <3468619937@qq.com>
2025-10-14 15:45:38 +0800

    fix:使用分步查询避免对象嵌套

--- node-api/routes/chapters.js
+++ node-api/routes/chapters.js
@@ -1,47 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Course, Category, Chapter, User } = require('../models');
-const { success, failure } = require('../utils/responses');
+const { Course, Category, Chapter, User } = require("../models");
+const { success, failure } = require("../utils/responses");
 const { NotFoundError } = require("../utils/errors");
 
 /**
  * 查询章节详情
  * GET /chapters/:id
  */
-router.get('/:id', async function (req, res) {
+router.get("/:id", async function (req, res) {
   try {
     const { id } = req.params;
-    const condition = {
-      attributes: { exclude: ['CourseId'] },
-      include: [
-        {
-          model: Course,
-          as: 'course',
-          attributes: ['id', 'name'],
-          include: [
-            {
-              model: User,
-              as: 'user',
-              attributes: ['id', 'username', 'nickname', 'avatar', 'company'],
-            }
-          ]
-        }
-      ]
-    };
+    // const condition = {
+    //   attributes: { exclude: ['CourseId'] },
+    //   include: [
+    //     {
+    //       model: Course,
+    //       as: 'course',
+    //       attributes: ['id', 'name'],
+    //       include: [
+    //         {
+    //           model: User,
+    //           as: 'user',
+    //           attributes: ['id', 'username', 'nickname', 'avatar', 'company'],
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // };
 
-    const chapter = await Chapter.findByPk(id, condition);
+    const chapter = await Chapter.findByPk(id, {
+      attributes: { exclude: ["CourseId"] },
+    });
     if (!chapter) {
-      throw new NotFoundError(`ID: ${ id }的章节未找到。`)
+      throw new NotFoundError(`ID: ${id}的章节未找到。`);
     }
+
+    const course = await chapter.getCourse({
+      attribute: ["id", "name", "userId"],
+    });
+    // 查询课程关联的用户
+    const user = await course.getUser({
+      attributes: ["id", "username", "nickname", "avatar", "company"],
+    });
 
     // 同属一个课程的所有章节
     const chapters = await Chapter.findAll({
-      attributes: { exclude: ['CourseId', 'content'] },
+      attributes: { exclude: ["CourseId", "content"] },
       where: { courseId: chapter.courseId },
-      order: [['rank', 'ASC'], ['id', 'DESC']]
+      order: [
+        ["rank", "ASC"],
+        ["id", "DESC"],
+      ],
     });
 
-    success(res, '查询章节成功。', { chapter, chapters });
+    success(res, "查询章节成功。", { chapter, course, user, chapters });
   } catch (error) {
     failure(res, error);
   }
